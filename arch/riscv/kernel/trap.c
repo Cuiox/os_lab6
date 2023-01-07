1b851844bc9b8bc30b76657fe70d30b08af65865
Cuiox <1806972779@qq.com>
2023-01-07 15:14:54 +0800

    1

--- arch/riscv/kernel/trap.c
+++ arch/riscv/kernel/trap.c
@@ -8,6 +8,9 @@ struct pt_regs {
     uint64_t x[32];
     uint64_t sepc;
     uint64_t sstatus;
+    uint64_t stval;
+    uint64_t sscratch;
+    uint64_t scause;
 };
 
     // 通过 `scause` 判断trap类型
@@ -30,15 +33,25 @@ void trap_handler(unsigned long scause, unsigned long sepc, struct pt_regs *regs
         // exception
         if(scause == 0x8){
             // ECALL_FROM_U_MODE
-            if(regs->x[17] == SYS_WRITE){
+            uint64_t sys_call_num = regs->x[17];
+            if(sys_call_num == SYS_WRITE){
                 uint64_t fd     = regs->x[10];
                 uint64_t buf    = regs->x[11];
                 uint64_t count  = regs->x[12];
                 sys_write((unsigned int)fd, (const char *)buf, (size_t)count);
-            }else if(regs->x[17] == SYS_GETPID){
+            }else if(sys_call_num == SYS_GETPID){
                 regs->x[10]     = sys_getpid();
+            }else{
+                printk("[S] Unhandled syscall: %lx", sys_call_num);
+                while (1);
             }
             regs->sepc += 4;
+        }else{
+            printk("[S] Unhandled trap, ");
+            printk("scause: %lx, ", scause);
+            printk("stval: %lx, ", regs->stval);
+            printk("sepc: %lx\n", regs->sepc);
+            while (1);
         }
     }
  
